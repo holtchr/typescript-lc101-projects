8d7ef8a0aa83fd5e6daa8c6ffd19fbd88c3122f1
Chris H <holtchr12@gmail.com>
2019-12-08 15:33:53 -0600

    finished exercise for typescript

--- exercises/part3.ts
+++ exercises/part3.ts
@@ -9,8 +9,10 @@ let milesPerKilometer: number = 0.621;
 
 // Code the "getDaysToLocation" function here:
 
-
-
-
+function getDaysToLocation(kilometersAway:number): number {
+    return((kilometersAway*milesPerKilometer)/speedMph/24);
+}
 
 // Call the function and print the outputs for the Mars trip and the moon trip:
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToMars)} days to reach Mars.`);
+console.log(`${spacecraftName} would take ${getDaysToLocation(kilometersToTheMoon)} days to reach the Moon.`);
---
+++ exercises/part4.js
@@ -0,0 +1,21 @@
+var kilometersToMars = 225000000;
+var kilometersToTheMoon = 384400;
+// Define your Spacecraft class here:
+var Spacecraft = /** @class */ (function () {
+    function Spacecraft(name, speedMph) {
+        this.milesPerKilometer = 0.621;
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    Spacecraft.prototype.getDaysToLocation = function (kilometersAway) {
+        var milesAway = kilometersAway * this.milesPerKilometer;
+        var hours = milesAway / this.speedMph;
+        return hours / 24;
+    };
+    return Spacecraft;
+}());
+// Create an instance of the class here:
+var spaceShuttle = new Spacecraft('Determination', 17500);
+// Print two outputs - one for the trip to Mars and one for the trip to the moon.
+console.log(spaceShuttle.name + " would take " + spaceShuttle.getDaysToLocation(kilometersToMars) + " days to reach Mars.");
+console.log(spaceShuttle.name + " would take " + spaceShuttle.getDaysToLocation(kilometersToTheMoon) + " days to reach the Moon.");
