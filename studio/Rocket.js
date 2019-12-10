a1991f3fa13b7338724b17100eda5ddafd88c1cc
Chris H <holtchr12@gmail.com>
2019-12-09 19:15:22 -0600

    finished studio for typescript

---
+++ studio/Rocket.js
@@ -0,0 +1,54 @@
+"use strict";
+exports.__esModule = true;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        //Returns the sum of all items using each item's massKg property
+        var summedMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            summedMass += items[i].massKg;
+        }
+        return summedMass;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        //Uses this.sumMass to return the combined mass of this.astronauts and this.cargoItems
+        return (this.sumMass(this.astronauts) + this.sumMass(this.cargoItems));
+    };
+    Rocket.prototype.canAdd = function (item) {
+        //Returns true if this.currentMassKg() + item.massKg <= this.totalCapacityKg
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        //Uses this.canAdd() to see if another item can be added.
+        //If true, adds cargo to this.cargoItems and returns true.
+        //If false, returns false.
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        //Uses this.canAdd() to see if another astronaut can be added.
+        //If true, adds astronaut to this.astronauts and returns true.
+        //If false, returns false.
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
